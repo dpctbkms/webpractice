d41a188c1854a2dd63303c78acf049b825ed2bad
dpctbkms <dpctbkms@users.noreply.github.com>
2021-02-05 04:01:43 +0900

    u

---
+++ templateLiteral.js
@@ -0,0 +1,6 @@
+var name = 'kim';
+var letter = `Dear ${1+1} 
+ afdljalkneflnaelfnalsdnflasd sadf sadlfjaskdljf askdljfa
+kd kdjfa kdjfaskdlfja skdljf kjskdl jlajfkdskdafskdajf lajskdf lajskd ljskad flka nflansdflasdn. 
+${name}`
+console.log(letter);
